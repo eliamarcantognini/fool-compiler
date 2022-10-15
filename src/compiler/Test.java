99c0f193887381680dda0b214974f51a94f6bb41
Elia Marcantognini <elia.marcantognini@gmail.com>
2022-10-15 15:08:19 +0200

    Test SymbolTableASTVisitor

--- src/compiler/Test.java
+++ src/compiler/Test.java
@@ -27,11 +27,11 @@ public class Test {
     	Node ast = visitor.visit(st);
     	System.out.println();
 
-//    	System.out.println("Enriching AST via symbol table.");
-//    	SymbolTableASTVisitor symtableVisitor = new SymbolTableASTVisitor(true);
-//    	symtableVisitor.visit(ast);
-//    	System.out.println("You had "+symtableVisitor.stErrors+" symbol table errors.\n");
-//
+    	System.out.println("Enriching AST via symbol table.");
+    	SymbolTableASTVisitor symtableVisitor = new SymbolTableASTVisitor(true);
+    	symtableVisitor.visit(ast);
+    	System.out.println("You had "+symtableVisitor.stErrors+" symbol table errors.\n");
+
     	System.out.println("Visualizing Enriched AST.");
     	new PrintEASTVisitor().visit(ast);
     	System.out.println();
