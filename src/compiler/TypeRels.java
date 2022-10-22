678e3b53d93eb0291f15040dc07cfef7b2c628c5
Elia Marcantognini <elia.marcantognini@gmail.com>
2022-10-22 13:15:10 +0200

    Fix error in TypeRels

--- src/compiler/TypeRels.java
+++ src/compiler/TypeRels.java
@@ -17,10 +17,6 @@ public class TypeRels {
         if ((a instanceof EmptyTypeNode) && (b instanceof RefTypeNode))
             return true;
 
-        // null can't be a super class
-        if (b instanceof EmptyTypeNode)
-            return false;
-
         if (a instanceof RefTypeNode cA && b instanceof RefTypeNode cB) {
             // a is a class ref and b is a class ref
             // if a and b are the same class -> a is a subtype of b
@@ -36,7 +32,7 @@ public class TypeRels {
             if (fA.parlist.size() != fB.parlist.size()) return false;
             // check if the parameters of a are subtypes of the parameters of b
             for (int i = 0; i < fA.parlist.size(); i++) {
-                if (!isSubtype(fA.parlist.get(i), fB.parlist.get(i)))
+                if (!isSubtype(fB.parlist.get(i), fA.parlist.get(i)))
                     return false; // contravariance of parameters' type
             }
             return true;
